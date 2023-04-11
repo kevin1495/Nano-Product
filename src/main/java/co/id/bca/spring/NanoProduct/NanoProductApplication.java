272a1f248fd602a708c6828f7f95102daceb3f06
kevin1495 <kevin_anggawijaya@yahoo.com>
2023-04-11 15:57:09 +0700

    start spring io

---
+++ src/main/java/co/id/bca/spring/NanoProduct/NanoProductApplication.java
@@ -0,0 +1,13 @@
+package co.id.bca.spring.NanoProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanoProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanoProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/id/bca/spring/NanoProduct/NanoProductApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.bca.spring.NanoProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanoProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
